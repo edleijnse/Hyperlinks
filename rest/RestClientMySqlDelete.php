8dcd600acf9ebd66c8bab87013eeb79d8f7ceb7b
Ed Leijnse <ed@leijnse.info>
2022-04-30 14:22:25 +0200

    hyperlinks version 1.3 MySql implemented (select, insert, delete) with corrected encoding/decoding

---
+++ rest/RestClientMySqlDelete.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+<body>
+<?php
+$IDErr = $groupErr = $categoryErr = $webdescriptionErr = $websiteErr = "";
+$ID = $group = $category = $webdescription = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["ID"])) {
+        $IDErr = "ID is required";
+    } else {
+        $ID = $_POST["ID"];
+    }
+    if (empty($_POST["group"])) {
+        $groupErr = "group is required";
+    } else {
+        $group = $_POST["group"];
+        // }
+    }
+    if (empty($_POST["category"])) {
+        $categoryErr = "category is required";
+    } else {
+        $category = $_POST["category"];
+    }
+    if (empty($_POST["webdescription"])) {
+        $webdescriptionErr = "webdescription is required";
+    } else {
+        $webdescription = $_POST["webdescription"];
+    }
+    if (empty($_POST["website"])) {
+        $websiteErr = "website is required";
+    } else {
+        $website = $_POST["website"];
+    }
+
+}
+if (function_exists('test_input')) {
+    echo "Function Exists";
+} else {
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+?>
+<h2>Delete hyperlinks</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="">
+    ID______________: <input type="text" name="ID" value="<?php echo $ID; ?>">
+    <span class="error">* <?php echo $IDErr; ?></span>
+    <br><br>
+
+    <input type="submit" name="submit" value="Submit">
+</form>
+<?php
+echo "<br>";
+if (empty($_POST["ID"])) {
+    echo "<h2>enter missing fields</h2>";
+} else {
+    echo "<h2>delete row with id: ". $ID . "</h2>";
+    $url = 'https://leijnse.info/hyperlinks/rest/Restcontroller.php/?command=deletemysql';
+    $url = $url . '&ID=' . $ID;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_json = curl_exec($ch);
+    curl_close($ch);
+    $myResponce = "";
+    $myResponce = json_decode($response_json, true);
+    echo "<h2>Result</h2>";
+    echo $myResponce;
+}
+
+?>
+</body>
+</html>
---
+++ rest/RestClientMySqlInput.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+<body>
+<?php
+$searchErr = "";
+$search = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["search"])) {
+        $nameErr = "search is required";
+    } else {
+        $search = $_POST["search"];
+        // check if name only contains letters and whitespace
+        // if (!preg_match("/^[a-zA-Z-' ]*$/",$search)) {
+        //    $searchErr = "Only letters and white space allowed";
+        // }
+    }
+
+}
+if (function_exists('test_input')) {
+    echo "Function Exists";
+} else {
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+?>
+<h2>Search hyperlinks</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="">
+    Search for: <input type="text" name="search" value="<?php echo $search; ?>">
+    <span class="error">* <?php echo $searchErr; ?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
+<?php
+echo "<h2>Searched for:</h2>";
+echo $search;
+echo "<br>";
+$url = 'https://leijnse.info/hyperlinks/rest/Restcontroller.php/?command=allmysql&count=900&from=0&search=' . urlencode($search);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HTTPGET, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response_json = curl_exec($ch);
+curl_close($ch);
+$hyperlinks = array();
+$hyperlinks = json_decode($response_json, true,512);
+echo "<h2>Search Results</h2>";
+echo "<table>";
+foreach ($hyperlinks as $hyperlink) {
+    if (isset($hyperlink['ID'])) {
+        echo "<tr>";
+        try {
+            echo "<th>";
+            echo $hyperlink['ID'];
+            echo "</th>";
+            echo "<th>";
+            echo $hyperlink['group'];
+            echo "</th>";
+            echo "<th>";
+            echo $hyperlink['category'];
+            echo "</th>";
+            echo "<th>";
+            echo $hyperlink['webdescription'];
+            echo "</th>";
+            echo "<th>";
+            echo "<a href=" . $hyperlink['website'] . ">" . $hyperlink['website'] . "</a>";
+            // echo $hyperlink['website'];
+            echo "</th>";
+            /* print($hyperlink['ID']) . ", " . $hyperlink['group'] . ", " . $hyperlink['category']
+                 . ", " . $hyperlink['webdescription']
+                 . ", " . $hyperlink['website']
+                 . PHP_EOL;*/
+
+        } catch (TypeError $e) {
+
+        } finally {
+
+        }
+        echo "</tr>";
+    }
+}
+echo "</table>";
+?>
+</body>
+</html>
