577bca7275b102bbea2e28f4e4b1fbc4ef329155
edleijnse <ed@leijnse.info>
2023-12-10 13:45:56 +0100

    MySql local test

---
+++ mysqlsample/readHyperlinks.php
@@ -0,0 +1,40 @@
+<?php
+// $servername = "{SERVERNAME}";
+$servername = "192.168.0.37";
+//$username = "{USERNAME}";
+$username = "edlei";
+// $password = "{PASSWORD}";
+$password = "Rax03Dam";
+$dbname = "mysql";
+
+try {
+    // Create PDO instance
+    $conn = new PDO("mysql:host={$servername};dbname={$dbname}", $username, $password);
+
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // SQL Query
+    $sql = "SELECT * FROM hyperlinks";
+
+    // Prepare statement
+    $stmt = $conn->prepare($sql);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // Fetch all result
+    $hyperlinks = $stmt->fetchAll();
+
+    // Display the result
+    foreach($hyperlinks as $hyperlink) {
+        echo "ID: " . $hyperlink['ID'] . ", Webgroup: " . $hyperlink['webgroup'] . ", Webcategory: " . $hyperlink['webcategory'] . ", Webdescription: " . $hyperlink['webdescription'] . ", Website: " . $hyperlink['website'] . "\n";
+    }
+
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+// Close the connection
+$conn = null;
+?>
