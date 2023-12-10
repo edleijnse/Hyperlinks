6f3df5d1d1a452a2be65779b8bde9c1d0b2f0f52
edleijnse <ed@leijnse.info>
2023-12-10 17:41:16 +0100

    Switch if an URL is not working - refactored

--- rest/RestClientMySqlInput.php
+++ rest/RestClientMySqlInput.php
@@ -8,12 +8,14 @@
 
         table {
             font-family: arial, sans-serif;
+            font-weight: normal;
             border-collapse: collapse;
             width: 100%;
         }
 
         td, th {
             border: 1px solid #dddddd;
+            font-weight: normal;
             text-align: left;
             padding: 8px;
         }
@@ -24,104 +26,83 @@
     </style>
 </head>
 <body>
-<?php
-$searchErr = "";
-$search = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["search"])) {
-        $nameErr = "search is required";
-    } else {
-        $search = $_POST["search"];
-        // check if name only contains letters and whitespace
-        // if (!preg_match("/^[a-zA-Z-' ]*$/",$search)) {
-        //    $searchErr = "Only letters and white space allowed";
-        // }
-    }
-
-}
-if (function_exists('test_input')) {
-    echo "Function Exists";
-} else {
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-}
-
-?>
 <h2>Search hyperlinks</h2>
-<p><span class="error">* required field</span></p>
+
 <form method="post" action="">
-    Search for: <input type="text" name="search" value="<?php echo $search; ?>">
-    <span class="error">* <?php echo $searchErr; ?></span>
+    Search for: <input type="text" name="search" value="<?php echo htmlspecialchars($_POST["search"] ?? ''); ?>">
     <br><br>
     <input type="submit" name="submit" value="Submit">
 </form>
+
 <?php
-echo "<h2>Searched for:</h2>";
-echo $search;
-echo "<br>";
-$url = 'https://leijnsse.info/hyperlinks/rest/Restcontroller.php/?command=allmysql&count=900&from=0&search=' . urlencode($search);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HTTPGET, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response_json = curl_exec($ch);
-curl_close($ch);
-$hyperlinks = array();
-$hyperlinks = json_decode($response_json, true, 512);
-if ($hyperlinks === null){
-    $url = 'https://leijnse.info/hyperlinks/rest/Restcontroller.php/?command=allmysql&count=900&from=0&search=' . urlencode($search);
-    // $url = 'http://192.168.0.37/rest/Restcontroller.php/?command=allmysql&count=900&from=0&search=' . urlencode($search);
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["search"])) {
+
+    // call function with $url as parameter
+    $search = filter_input(INPUT_POST, 'search', FILTER_SANITIZE_STRING);
+    echo "<h2>Searched for: $search</h2>";
+    $url = 'https://leijnsse.info/hyperlinks/rest/Restcontroller.php/?command=allmysql&count=900&from=0&search=' . urlencode($search);
+    $hyperlinks = fetchHyperlinks($url);
+    if ($hyperlinks === null) {
+        // retry with other URL
+        $url = 'https://leijnse.info/hyperlinks/rest/Restcontroller.php/?command=allmysql&count=900&from=0&search=' . urlencode($search);
+        $hyperlinks = fetchHyperlinks($url);
+    }
+    if ($hyperlinks === null) {
+        echo "Error fetching or decoding hyperlinks.";
+    } else {
+        displayHyperlinksTable($hyperlinks);
+    }
+}
+function fetchHyperlinks($url) {
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPGET, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response_json = curl_exec($ch);
     curl_close($ch);
-    $hyperlinks = array();
-    $hyperlinks = json_decode($response_json, true, 512);
+    return json_decode($response_json, true, 512);
 }
-echo "<h2>Search Results</h2>";
-echo "<table>";
 
-foreach ($hyperlinks as $hyperlink) {
-    if (isset($hyperlink['ID'])) {
-        echo "<tr>";
-        try {
-            echo "<th>";
-            echo $hyperlink['group'];
-            echo "</th>";
-            echo "<th>";
-            echo $hyperlink['category'];
-            echo "</th>";
-            echo "<th>";
-            echo "<a href=" . $hyperlink['website'] . ">" . $hyperlink['webdescription'] . "</a>";
-            echo "</th>";
-            echo "<th>";
-            echo "<a href=" . "https://tagger.biz/hyperlink-update/?ID=" . $hyperlink['ID']
-                . "&category=" . urlencode($hyperlink["category"])
-                . "&group=" . urlencode($hyperlink["group"])
-                . "&webdescription=" . urlencode($hyperlink["webdescription"])
-                . "&website=" . urlencode($hyperlink["website"])
-                . ">" . "update" . "</a>";
-            echo "</th>";
-            echo "<th>";
-            echo "<a href=" . "https://tagger.biz/hyperlink-delete-2/?ID=" . $hyperlink['ID']
-                . ">" . "delete" . "</a>";
-            echo "</th>";
 
-        } catch (TypeError $e) {
+function displayHyperlinksTable($hyperlinks) {
+    echo "<h2>Search Results</h2>";
+    echo "<table>";
 
-        } finally {
+    foreach ($hyperlinks as $hyperlink) {
+        if (isset($hyperlink['ID'])) {
+            echo "<tr>";
+            try {
+                echo "<th>";
+                echo $hyperlink['group'];
+                echo "</th>";
+                echo "<th>";
+                echo $hyperlink['category'];
+                echo "</th>";
+                echo "<th>";
+                echo "<a href=" . $hyperlink['website'] . ">" . $hyperlink['webdescription'] . "</a>";
+                echo "</th>";
+                echo "<th>";
+                echo "<a href=" . "https://tagger.biz/hyperlink-update/?ID=" . $hyperlink['ID']
+                    . "&category=" . urlencode($hyperlink["category"])
+                    . "&group=" . urlencode($hyperlink["group"])
+                    . "&webdescription=" . urlencode($hyperlink["webdescription"])
+                    . "&website=" . urlencode($hyperlink["website"])
+                    . ">" . "update" . "</a>";
+                echo "</th>";
+                echo "<th>";
+                echo "<a href=" . "https://tagger.biz/hyperlink-delete-2/?ID=" . $hyperlink['ID']
+                    . ">" . "delete" . "</a>";
+                echo "</th>";
 
+            } catch (TypeError $e) {
+
+            } finally {
+
+            }
+            echo "</tr>";
         }
-        echo "</tr>";
     }
+    echo "</table>";
 }
-echo "</table>";
 ?>
 </body>
 </html>
