e34195980499306ed94e39f1ab4baec16e3e662b
edleijnse <ed@leijnse.info>
2023-12-11 18:55:09 +0100

    Visually more appealing style in head.php

--- chatbot/head.php
+++ chatbot/head.php
@@ -1,30 +1,55 @@
 <head>
     <title>ChatGPT client</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-color: #f7f7f7;
+            color: #444444;
+            margin: 0;
+            padding: 0;
+        }
         .input {
-    font-size: 24px;
-            color: royalblue;
-    text-align: start;
+            font-size: 24px;
+            color: #4054b2;
+            text-align: start;
+            padding: 10px;
+            border-radius: 5px;
         }
         .list {
-    font-size: 38px;
-            color: goldenrod;
+            font-size: 38px;
+            color: #d4af37;
+            margin-top: 20px;
+            padding: 10px;
+            border-radius: 5px;
         }
         .output {
-    font-size: 24px;
-            color: dimgray;
+            background-color: #f7f7f7;
+            color: #707070;
+            font-size: 24px;
+            padding: 10px;
+            border-radius: 5px;
         }
         .ask {
-    font-size: 64px;
+            font-size: 64px;
+            color: #3b5998;
         }
         .heading {
-    font-size: 38px;
+            font-size: 38px;
+            font-weight: 500;
+            color: #3b5998;
         }
         .error-message {
-    font-size: 38px;
+            color: #ff6161;
+            background-color: #ffe6e6;
+            padding: 10px;
+            border-radius: 5px;
+            font-size: 38px;
+            margin: 20px 0;
         }
         .large-font {
-    font-size: 72px;
+            font-size: 72px;
+            color: #3b5998;
         }
     </style>
 </head>
